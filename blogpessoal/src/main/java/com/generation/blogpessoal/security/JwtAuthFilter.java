54652006d061acea9558ca2f84ea2edea7809bed
Thiago Reis <reiscorithi@gmail.com>
2023-09-13 01:01:17 -0300

    Alteração de Dependencia

--- blogpessoal/src/main/java/com/generation/blogpessoal/security/JwtAuthFilter.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/security/JwtAuthFilter.java
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -20,6 +21,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+@Component
 public class JwtAuthFilter extends OncePerRequestFilter {
 	
 	@Autowired
